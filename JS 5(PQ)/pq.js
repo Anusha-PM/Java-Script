aefd1ac020055ebb13538f8d235f850997080e69
Anusha <anushapm758@gmail.com>
2026-08-21 22:01:32 +0530

    object literals

---
+++ JS 5(PQ)/pq.js
@@ -0,0 +1,27 @@
+// 1st Question
+let dice = Math.floor(Math.random()*6)+1;
+console.log(dice);
+
+// 2nd Question
+const car ={
+    name : "BMW",
+    model: 2026,
+    color:"black",
+
+};
+console.log(car.name);
+
+// 3rd Question
+
+const person ={
+    name:"anu",
+    age:22,
+   city:"hassan",
+
+};
+
+person.city="New York";
+person.country="United States";
+
+console.log(person);
+
